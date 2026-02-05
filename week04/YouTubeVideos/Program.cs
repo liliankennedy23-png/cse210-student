8066d8e9269b63d2e9e569c221ccee57fc0294fe
Lillian Kennedy <lnaftal@byupathway.edu>
2026-02-05 12:32:23 -0500

    create week05/Homework/MathAssignment.cs

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,100 +1,44 @@
 using System;
 using System.Collections.Generic;
 
-public class Comment
-{
-    public string CommenterName { get; set; }
-    public string Text { get; set; }
-
-    public Comment(string commenterName, string text)
-    {
-        CommenterName = commenterName;
-        Text = text;
-    }
-
-    public override string ToString() => $"{CommenterName}: {Text}";
-}
-
-public class Video
-{
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public int LengthSeconds { get; set; }
-    private List<Comment> Comments { get; set; }
-
-    public Video(string title, string author, int lengthSeconds)
-    {
-        Title = title;
-        Author = author;
-        LengthSeconds = lengthSeconds;
-        Comments = new List<Comment>();
-    }
-
-    public void AddComment(Comment comment) => Comments.Add(comment);
-    public int GetNumberOfComments() => Comments.Count;
-    public void DisplayComments()
-    {
-        foreach (var comment in Comments)
-        {
-            Console.WriteLine("  - " + comment);
-        }
-    }
-}
-
 class Program
 {
     static void Main(string[] args)
     {
-        Random rnd = new Random();
-
-        string[] videoTitles = {
-            "C# Basics Tutorial",
-            "Top 10 YouTube Tips",
-            "Funny Cat Compilation",
-            "Travel Vlog: Japan",
-            "Fitness Workout Routine"
-        };
-
-        string[] authors = { "Alice", "Bob", "Charlie", "Diana", "Eve" };
-        string[] commenterNames = { "John", "Maria", "Steve", "Lily", "Tom", "Sara", "Nina" };
-        string[] sampleComments = {
-            "Loved this!", "Very helpful.", "Could be better.", "Amazing content!", "Thanks for sharing.",
-            "This made my day!", "I learned something new.", "I disagree with this point.", "Awesome video!"
-        };
-
         List<Video> videos = new List<Video>();
 
-        // Create 3-5 random videos
-        int numVideos = rnd.Next(3, 6);
-        for (int i = 0; i < numVideos; i++)
-        {
-            string title = videoTitles[rnd.Next(videoTitles.Length)];
-            string author = authors[rnd.Next(authors.Length)];
-            int length = rnd.Next(60, 1200); // 1 to 20 minutes in seconds
-            Video video = new Video(title, author, length);
+        Video video1 = new Video("Learning C#", "Code Academy", 600);
+        video1.AddComment(new Comment("Anna", "Very helpful!"));
+        video1.AddComment(new Comment("James", "Clear explanation."));
+        video1.AddComment(new Comment("Lily", "Loved this video."));
 
-            // Random number of comments per video
-            int numComments = rnd.Next(3, 8);
-            for (int j = 0; j < numComments; j++)
+        Video video2 = new Video("OOP Basics", "Tech World", 480);
+        video2.AddComment(new Comment("Mark", "Great examples."));
+        video2.AddComment(new Comment("Sara", "Easy to understand."));
+        video2.AddComment(new Comment("Tom", "Thanks!"));
+
+        Video video3 = new Video("Abstraction Explained", "Dev Simplified", 720);
+        video3.AddComment(new Comment("Chris", "This helped a lot."));
+        video3.AddComment(new Comment("Emily", "Perfect timing."));
+        video3.AddComment(new Comment("Noah", "Nice breakdown."));
+
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+
+        foreach (Video video in videos)
+        {
+            Console.WriteLine($"Title: {video.GetTitle()}");
+            Console.WriteLine($"Author: {video.GetAuthor()}");
+            Console.WriteLine($"Length: {video.GetLength()} seconds");
+            Console.WriteLine($"Number of Comments: {video.GetCommentCount()}");
+
+            foreach (Comment comment in video.GetComments())
             {
-                string commenter = commenterNames[rnd.Next(commenterNames.Length)];
-                string text = sampleComments[rnd.Next(sampleComments.Length)];
-                video.AddComment(new Comment(commenter, text));
+                Console.WriteLine($"- {comment.GetCommenterName()}: {comment.GetCommentText()}");
             }
 
-            videos.Add(video);
-        }
-
-        // Display videos
-        foreach (var video in videos)
-        {
-            Console.WriteLine($"Title: {video.Title}");
-            Console.WriteLine($"Author: {video.Author}");
-            Console.WriteLine($"Length (seconds): {video.LengthSeconds}");
-            Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
-            Console.WriteLine("Comments:");
-            video.DisplayComments();
-            Console.WriteLine(new string('-', 60));
+            Console.WriteLine();
         }
     }
 }
